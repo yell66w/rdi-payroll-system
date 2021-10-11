02cadb38cb54630974d0a74668f1760f7b4e9046
Glendell <dellbringino@gmail.com>
2021-10-11 10:43:20 +0800

    Default theme for fontsizes

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -13,4 +13,19 @@ export const theme = {
     avenirBook: "Avenir-Book",
     avenirRoman: "Avenir-Roman",
   },
+  fontSizes: {
+    xs: "0.75rem", //12px
+
+    sm: "0.875rem", //14px
+
+    md: "1rem", //16px
+
+    lg: "1.125rem", //18px
+
+    xl: "1.25rem", //20px
+
+    xl2: "1.5rem", //24px
+
+    xxl: "2.25rem", //36px
+  },
 };
