7eab41d01adc23f1baee1a5cd26da88ea9171592
Glendell <dellbringino@gmail.com>
2021-10-19 23:27:58 +0800

    Implement login

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -49,7 +49,10 @@ export const authSlice = createSlice({
     },
     [signinUser.fulfilled]: (state, { payload }) => {
       state.username = payload.username;
+<<<<<<< HEAD
       state.role = payload.role;
+=======
+>>>>>>> Implement login
       state.isFetching = false;
       state.isSuccess = true;
 
