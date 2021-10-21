f81cf4c508cfcb1c4307518d3ec03a678a99994b
Ryan Tillaman <ryanbuenaventura321@gmail.com>
2021-10-21 21:20:08 +0800

    app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,14 @@ import EncoderPage from "pages/encoder";
 import AuditorPage from "pages/Auditor/index.jsx";
 import LoginPage from "pages/Login";
 
-
 function App() {
   return (
     <>
       <Switch>
         <Route exact path="/" component={IndexPage} />
         <Route path="/login/:name" component={LoginPage} />
-        <Route path="/encoder" component={EncoderPage} />        
-        <Route path="/auditor" component={AuditorPage} /> 
-        
+        <Route path="/encoder" component={EncoderPage} />
+        <Route path="/auditor" component={AuditorPage} />
       </Switch>
     </>
   );
