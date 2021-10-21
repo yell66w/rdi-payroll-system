b997f6da8fe9b29a32bf672ba82df33a710f8017
Ryan Tillaman <ryanbuenaventura321@gmail.com>
2021-10-21 22:08:53 +0800

    created a homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,12 @@
 import { Switch, Route } from "react-router-dom";
-import IndexPage from "pages/index";
 import LoginPage from "pages/Login";
-import MainPage from "containers/Main";
-
+import HomePage from "pages/Home";
 function App() {
   return (
     <>
       <Switch>
-        <Route exact path="/" component={IndexPage} />
-        <Route path="/login/:name" component={LoginPage} />
-        <Route path="/encoder" component={MainPage} />
-        <Route path="/auditor" component={MainPage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/" component={HomePage} />
       </Switch>
     </>
   );
--- frontend/src/pages/Home/styles.js
+++ frontend/src/pages/Home/styles.js
@@ -1,7 +1,7 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const MainCan = styled.div`
-  display: flex;    
+  display: flex;
 `;
 
 export const MainRight = styled.div`
