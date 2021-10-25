3a049cb5955db1318fb6bb298c7ec469159aa68f
Ryan Tillaman <ryanbuenaventura321@gmail.com>
2021-10-25 20:19:39 +0800

    added seeders and modified seeders with relationships

---
+++ backend/src/seeders/20211022020759-insert-department.js
@@ -0,0 +1,32 @@
+"use strict";
+const { v4: uuidv4 } = require("uuid");
+const bcrypt = require("bcryptjs");
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const companies = await queryInterface.sequelize.query(
+      `SELECT id FROM companies;`
+    );
+
+    const companyRows = companies[0];
+
+    return await queryInterface.bulkInsert("departments", [
+      {
+        id: uuidv4(),
+        name: "Accounting",
+        created_at: new Date(),
+        updated_at: new Date(),
+        company_id: companyRows[0].id,
+      },
+      {
+        id: uuidv4(),
+        name: "IT",
+        created_at: new Date(),
+        updated_at: new Date(),
+        company_id: companyRows[0].id,
+      },
+    ]);
+  },
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("departments", null, {});
+  },
+};
