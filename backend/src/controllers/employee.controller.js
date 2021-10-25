5d251775f1cf802f7dbce68726d5b4562e59da21
Ryan Tillaman <ryanbuenaventura321@gmail.com>
2021-10-25 22:45:01 +0800

    added date filter inside employee controller

--- backend/src/controllers/employee.controller.js
+++ backend/src/controllers/employee.controller.js
@@ -1,3 +1,4 @@
+const { Op } = require("../models");
 const db = require("../models");
 const Employee = db.employee;
 
@@ -5,13 +6,23 @@ exports.findAll = async (req, res) => {
   const company = req.query.company;
   const department = req.query.department;
   const position = req.query.position;
+  const date_hired_from = req.query.date_hired_from;
+  const date_hired_to = req.query.date_hired_to;
+
   let options = { where: {} };
 
-  if (company) {
-    options.where.company_id = company;
-  }
+  if (company) options.where.company_id = company;
   if (department) options.where.department_id = department;
   if (position) options.where.position_id = position;
+
+  if (date_hired_from && date_hired_to) {
+    const start_date = new Date(date_hired_from);
+    const end_date = new Date(date_hired_to);
+    options.where.date_hired = {
+      [Op.between]: [start_date, end_date],
+    };
+  }
+
   options.include = [
     "company",
     "department",
