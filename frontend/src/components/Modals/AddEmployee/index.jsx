83b545023fa9f0ecf6db656c6aec6e8c75730ff3
Ryan Tillaman <ryanbuenaventura321@gmail.com>
2021-10-30 13:36:35 +0800

    replaced chakra modal with react-modal

--- frontend/src/components/Modals/AddEmployee/index.jsx
+++ frontend/src/components/Modals/AddEmployee/index.jsx
@@ -1,29 +1,12 @@
 import React from 'react';
-import {
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton
-} from '@chakra-ui/modal';
-import Button from 'components/Button';
+import ReactModal from 'react-modal';
+ReactModal.setAppElement('#root');
 
 const AddEmployee = ({ isOpen, onClose }) => {
   return (
-    <Modal bg="red" isOpen={isOpen} onClose={onClose}>
-      <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>Modal Title</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>Hello World</ModalBody>
-        <ModalFooter>
-          <Button onClick={onClose}>Close</Button>
-          <Button>Secondary Action</Button>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
+    <ReactModal isOpen={isOpen} contentLabel="Add Employee Modal">
+      <button onClick={onClose}>Close Modal</button>
+    </ReactModal>
   );
 };
 
