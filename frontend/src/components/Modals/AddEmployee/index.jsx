0623ad134011a5d08cd6271b295a9309db59348f
Ryan Tillaman <ryanbuenaventura321@gmail.com>
2021-10-30 13:23:15 +0800

    added chakraui-modal

--- frontend/src/components/Modals/AddEmployee/index.jsx
+++ frontend/src/components/Modals/AddEmployee/index.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton
+} from '@chakra-ui/react';
+import Button from 'components/Button';
 
-const AddEmployee = () => {
-  return <div></div>;
+const AddEmployee = ({ isOpen, onClose }) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Modal Title</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>Hello World</ModalBody>
+        <ModalFooter>
+          <Button onClick={onClose}>Close</Button>
+          <Button>Secondary Action</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 };
 
 export default AddEmployee;
