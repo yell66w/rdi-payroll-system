93c5a8b2c26233476164cac5df72f80d18d2d634
CloudyMushroom <jdejesus965@gmail.com>
2021-10-31 22:34:14 +0800

    Request Page

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -3,16 +3,19 @@ import Sidebar from 'components/Sidebar';
 import { MainCan, MainRight, WrapperRight } from './styles';
 import Link from 'components/Link';
 import { useLocation } from 'react-router';
+import routes from 'routes';
 
 const MainWrapper = ({ children, props }) => {
   const location = useLocation();
-
+  // encoder based sidebar
   const routesMap = new Map();
-  routesMap.set('/', 'PAYROLL');
+  routesMap.set('/', 'HUMAN RESOURCES');
+  routesMap.set('/payroll', 'PAYROLL');
   routesMap.set('/attendance', 'ATTENDANCE');
   routesMap.set('/employee-file', 'EMPLOYEE FILE');
-  routesMap.set('/for-approval', 'FOR APPROVAL');
-  routesMap.set('/reports', 'REPORTS');
+  routesMap.set('/memo', 'MEMO');
+  routesMap.set('/cash-advance', 'CASH ADVANCE');
+  routesMap.set('/request', 'REQUESTS');
 
   const headerName = routesMap.get(location.pathname);
 
@@ -20,11 +23,13 @@ const MainWrapper = ({ children, props }) => {
     <>
       <MainCan>
         <Sidebar>
-          <Link to={`/`}>PAYROLL</Link>
+          <Link to={`/`}>HUMAN RESOURCES</Link>
+          <Link to={`/payroll`}>PAYROLL</Link>
           <Link to={`/attendance`}>ATTENDANCE</Link>
           <Link to={`/employee-file`}>EMPLOYEE FILE</Link>
-          <Link to={`/for-approval`}>FOR APPROVAL</Link>
-          <Link to={`/reports`}>REPORTS</Link>
+          <Link to={`/memo`}>MEMO</Link>
+          <Link to={`/cash-advance`}>CASH ADVANCE</Link>
+          <Link to={`/request`}>REQUEST</Link>
         </Sidebar>
 
         <MainRight>
