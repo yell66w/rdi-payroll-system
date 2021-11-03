4120cebcde093594fc9ae38668eacbc676f2d2e0
Ryan Tillaman <ryanbuenaventura321@gmail.com>
2021-11-03 21:58:40 +0800

    button-size

--- frontend/src/routes/PublicRoute.js
+++ frontend/src/routes/PublicRoute.js
@@ -1,27 +1,18 @@
 import { useSelector } from 'react-redux';
-import { Route, Redirect, useHistory } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 
 const PublicRoute = ({ children, ...rest }) => {
-  const history = useHistory();
   const { isAuth } = useSelector((state) => state.auth);
-  {
-    /* TEMP FIX KO LANG TO */
-  }
-  const path = history.location.state?.from.pathname || '/';
+
   return (
     <Route
       {...rest}
-      render={({ location }) =>
+      render={() =>
         !isAuth ? (
           children
         ) : (
           <>
-            <Redirect
-              to={{
-                pathname: `${path}`,
-                state: { from: location }
-              }}
-            />
+            <Redirect to="/" />
           </>
         )
       }
