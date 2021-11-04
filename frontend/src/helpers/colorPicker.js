c6fd0f45c5c7f194903f492274b5c66594ef328f
Ryan Tillaman <ryanbuenaventura321@gmail.com>
2021-11-04 15:54:50 +0800

    mod to switch case

--- frontend/src/helpers/colorPicker.js
+++ frontend/src/helpers/colorPicker.js
@@ -1,27 +1,28 @@
 import { theme } from 'theme';
 
 export default (value, category) => {
-  if (category === 'STATUS') {
-    switch (value) {
-      case 'PROCESSED':
-        return theme.colors.green;
-      case 'UNPROCESSED':
-        return theme.colors.red;
-      default:
-        return theme.colors.gray;
-    }
-  } else if (category === 'APPROVAL_STATUS') {
-    switch (value) {
-      case 'PAID':
-        return theme.colors.green;
-      case 'INCOMPLETE':
-        return theme.colors.red;
-      case 'DELAYED':
-        return 'orange';
-      default:
-        return theme.colors.gray;
-    }
-  } else {
-    return theme.colors.violet;
+  switch (category) {
+    case 'STATUS':
+      switch (value) {
+        case 'PROCESSED':
+          return theme.colors.green;
+        case 'UNPROCESSED':
+          return theme.colors.red;
+        default:
+          return theme.colors.gray;
+      }
+    case 'APPROVAL_STATUS':
+      switch (value) {
+        case 'PAID':
+          return theme.colors.green;
+        case 'INCOMPLETE':
+          return theme.colors.red;
+        case 'DELAYED':
+          return 'orange';
+        default:
+          return theme.colors.gray;
+      }
+    default:
+      return theme.colors.violet;
   }
 };
